d81cd997490362df6fe1d3d5b06784f8503a37a4
EtienneMar <etienne.martinc@gmail.com>
2023-12-15 09:58:00 +0100

    Ajout de JavaDoc

--- ecommerce/src/main/java/com/android/ecommerce/generic/GenericCRUDController.java
+++ ecommerce/src/main/java/com/android/ecommerce/generic/GenericCRUDController.java
@@ -10,38 +10,73 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+/**
+ * Generic controller class implémentant les opérations CRUD pour un type d'entité donnée.
+ * Cette classe abstraite peut-être hérité dans un controller specific à une entité to .
+ *
+ * @param <T> Le type de l'entité, qui dois implémenter IGenericEntity.
+ */
 public abstract class GenericCRUDController<T extends IGenericEntity<T>>{
 
 	private final GenericCRUDService<T> genericService; 
-	
+
 	protected GenericCRUDController(GenericCRUDService<T> genericService) {
 		this.genericService = genericService;
 	}
-	
+
+	/**
+	 * Retourne toutes les tuples de la table .
+	 * 
+	 * @return ReponseEntity contenant la liste des tuples.
+	 */
 	@GetMapping("")
 	public ResponseEntity<List<T>> findAll(){
 		return ResponseEntity.ok(genericService.findAll()); 
 	}
-	
+
+	/**
+	 * Retourne un seul tuple par son ID.
+	 * 
+	 * @param id l'identifiant de l'entité retrouvée.
+	 * @return ReponseEntity contenant le tuples.
+	 */
 	@GetMapping("/{id}")
 	public ResponseEntity<T> get(@PathVariable Integer id) {
 		return ResponseEntity.ok(genericService.get(id));
 	}
-	
+
+	/**
+	 * Créer un nouveau tuple.
+	 * 
+	 * @param createNewRecord l'instance de l'entité à créer.
+	 * @return ResponseEntity contenenant l'instance crée.
+	 */
 	@PostMapping("")
 	public ResponseEntity<T> create(@RequestBody T createNewRecord) {
 		return ResponseEntity.ok(genericService.create(createNewRecord));
 	}
-	
+
+	/**
+	 * Modifie un tuple existant dans la base
+	 * 
+	 * @param updatedRecord le tuple existant avec les informations modifiée.
+	 * @return ResponseEntity contenant le tuple modifiée.
+	 */
 	@PutMapping("")
 	public ResponseEntity<T> update(@RequestBody T updatedRecord) {
 		return ResponseEntity.ok(genericService.update(updatedRecord));
 	}
-	
+
+	/**
+	 * Supprimer un tuple par son identifiant.
+	 * 
+	 * @param id l'identifiant du tuple à supprimer
+	 * @return ResponseEntity avec un message de confirmation de suppression.
+	 */
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> delete (@PathVariable Integer id){
 		genericService.delete(id);
 		return ResponseEntity.ok("Record deleted");
 	}
-	
+
 }
